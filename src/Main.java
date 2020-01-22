d7dd16bff6e1d139505a4684406a9161ed08f260
Daniel <doesdeos@gmail.com>
2020-01-22 11:50:32 +0300

    Added autoscale

--- src/Main.java
+++ src/Main.java
@@ -77,13 +77,17 @@ class Ising{
 
 class Gui {
     Ising M, tempState;
-    int N = 100, J = 1, scale = 5;
+    int N = 100, J = 1, scale, width, height;
     double H = 0;
+    JFrame frame;
     class DisplayGraphics extends JComponent {
         public void paintComponent(Graphics g) {
+            width = frame.getWidth();
+            height = frame.getHeight();
+            scale = (int)Math.round(Math.sqrt((double)width * height / (N * N)) + 0.5);
             super.paintComponent(g);
             g.setColor(new Color(198, 200, 238));
-            g.fillRect(0, 0, 500, 520);
+            g.fillRect(0, 0, width, height);
             for(int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     g.setColor(new Color(154, 149, 183));
@@ -94,19 +98,20 @@ class Gui {
             int alpha = 200;
             Color textbg = new Color(163, 11, 55, alpha);
             g.setColor(textbg);
-            g.fillRect(0, 470, 500, 30);
+            g.fillRect(0, height - 50, width, 50);
             g.setColor(new Color(252, 252, 255));
             g.setFont(new Font("Helvetica Neue", Font.BOLD, 16));
-            g.drawString("H: " + Enormalized,20,489);
+            g.drawString("H: " + Enormalized,10,height - 30);
         }
     }
 
     public void start() {
-        final JFrame frame = new JFrame();
+        frame = new JFrame();
         JPanel mainPanel = new JPanel(new FlowLayout());
         DisplayGraphics displayState = new DisplayGraphics();
         mainPanel.add(displayState);
-        frame.setSize(500, 520);
+        frame.setSize(500, 500);
+        frame.setResizable(false);
         frame.add(new DisplayGraphics());
         frame.setVisible(true);
 // Start simulation
